1f68a23cb37870849ca76c21462dc24587d373b2
Darth Kobe Alcon <darth.kobe.alcon@accenture.com>
2025-08-22 01:31:20 +0000

    Added functionalities

--- project1/webapp/controller/EditOrder.controller.js
+++ project1/webapp/controller/EditOrder.controller.js
@@ -1,13 +1,36 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/UIComponent"
-], function (Controller, UIComponent) {
+    "sap/ui/core/UIComponent",
+    "sap/m/MessageBox"
+
+], function (Controller, UIComponent, MessageBox) {
     "use strict";
 
     return Controller.extend("com.ui5.trng.project1.controller.EditOrder", {
 
         onInit: function () {
-            
+
+        },
+        onSavePress: function () {
+            var that = this;
+            MessageBox.confirm("Are you sure you want to Save these changes?", {
+                title: "Confirm Save",
+                actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                onClose: function (oAction) {
+                    if (oAction === MessageBox.Action.YES) {
+                        that._updateOrder();
+                    }
+                }
+            });
+        },
+        _navigateToDetails: function (sOrderNumber) {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteDetailsOrder", {
+                orderPath: sOrderNumber
+            });
         }
+
+
+
     });
 });
