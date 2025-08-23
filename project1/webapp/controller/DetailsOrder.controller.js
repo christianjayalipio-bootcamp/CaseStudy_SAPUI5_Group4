e07e47e98af3c3d559d2535df777ed3301ed3e03
Christian Jay Alipio <christian.jay.alipio@accenture.com>
2025-08-23 15:26:46 +0000

    this an update for the details order and EditOrder

--- project1/webapp/controller/DetailsOrder.controller.js
+++ project1/webapp/controller/DetailsOrder.controller.js
@@ -2,7 +2,7 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
     "com/ui5/trng/project1/controller/formatter"
-], function(Controller, UIComponent, formatter) {
+], function (Controller, UIComponent, formatter) {
     "use strict";
 
     return Controller.extend("com.ui5.trng.project1.controller.DetailsOrder", {
@@ -14,31 +14,34 @@ sap.ui.define([
             oRouter.getRoute("RouteDetailsOrder").attachPatternMatched(this._onObjectMatched, this);
         },
 
-        onEditPress: function() {
-            // handle edit logic here
+        onEditPress: function () {
+            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            var sOrderId = this.getView().getBindingContext().getProperty("OrderID");
+            oRouter.navTo("RouteEditOrder", {
+                orderId: sOrderId
+            });
         },
 
-        onCancelPress: function() {
+        onCancelPress: function () {
             // navigate back to main view
             var oRouter = UIComponent.getRouterFor(this);
             oRouter.navTo("RouteMainView");
         },
+
         _onObjectMatched: function (oEvent) {
-    var sOrderPath = decodeURIComponent(oEvent.getParameter("arguments").orderPath);
+            var sOrderPath = decodeURIComponent(oEvent.getParameter("arguments").orderPath);
 
-    this.getView().bindElement({
-        path: "/" + sOrderPath,
-        events: {
-            dataRequested: function () {
-                this.getView().setBusy(true);
-            }.bind(this),
-            dataReceived: function () {
-                this.getView().setBusy(false);
-            }.bind(this)
+            this.getView().bindElement({
+                path: "/" + sOrderPath,
+                events: {
+                    dataRequested: function () {
+                        this.getView().setBusy(true);
+                    }.bind(this),
+                    dataReceived: function () {
+                        this.getView().setBusy(false);
+                    }.bind(this)
+                }
+            });
         }
-    });
-}
-
-
     });
 });
--- project1/webapp/controller/EditOrder.controller.js
+++ project1/webapp/controller/EditOrder.controller.js
@@ -5,9 +5,28 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("com.ui5.trng.project1.controller.EditOrder", {
-
         onInit: function () {
-            
+            var oRouter = UIComponent.getRouterFor(this);
+            oRouter.getRoute("RouteEditOrder").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            var sOrderId = oEvent.getParameter("arguments").orderId;
+
+            // Bind the whole view to the selected order
+            var sPath = "/Orders(" + sOrderId + ")"; // wrap sOrderId with quotes if it's string
+            this.getView().bindElement({
+                path: sPath,
+                parameters: {
+                    expand: "Products"
+                }
+            });
+        },
+
+        onCancelPress: function () {
+            UIComponent.getRouterFor(this).navTo("RouteDetailsOrder", {
+                orderId: this.getView().getBindingContext().getProperty("OrderID")
+            });
         }
     });
 });
